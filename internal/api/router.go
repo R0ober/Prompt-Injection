4a4f1d6e9f7809233991d896748145002b0eb646
R0ober <robin.karhu4@gmail.com>
2026-03-27 12:17:33 +0100

    init router

---
+++ internal/api/router.go
@@ -0,0 +1,47 @@
+package api
+
+import (
+	"encoding/json"
+	"net/http"
+	"temp-name/internal/auth"
+	"temp-name/internal/db"
+)
+
+func NewRouter(database *db.DB) http.Handler {
+	mux := http.NewServeMux()
+
+	mux.Handle("POST api/login", loginHandler(database))
+
+	return mux
+}
+
+func loginHandler(database *db.DB) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		var LoginBody struct {
+			Username string `json:"username"`
+			Password string `json:"password"`
+		}
+		err := json.NewDecoder(r.Body).Decode(&LoginBody)
+		if err != nil {
+			http.Error(w, "bad request", http.StatusBadRequest)
+			return
+		}
+		tokenString, err := auth.Login(database, LoginBody.Username, LoginBody.Password)
+		if err != nil {
+			http.Error(w, "invalid credentials", http.StatusUnauthorized)
+			return
+		}
+		// browsers
+		http.SetCookie(w, &http.Cookie{
+			Name:     "token",
+			Value:    tokenString,
+			Path:     "/",
+			HttpOnly: true, // js kan inte läsa token xss protection
+			SameSite: http.SameSiteStrictMode,
+		})
+		// mobile apps/ api
+		w.Header().Set("Content-Type", "application/json")
+		json.NewEncoder(w).Encode(map[string]string{"token": tokenString})
+
+	}
+}
